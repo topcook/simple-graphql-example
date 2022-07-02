05fed7227e073115c18235d5e8f87bb7774d18c7
topcook <codecooker1996@gmail.com>
2022-07-02 04:39:30 -0400

    query with enum paramteters

--- resolvers.js
+++ resolvers.js
@@ -3,25 +3,29 @@ const Query = {
     greeting: () => {
         return "hello from  TutorialsPoint !!!"
     },
-        
+
     students: () => db.students.list(),
-    
+
     //resolver function for studentbyId
     studentById: (root, args, context, info) => {
         //args will contain parameter passed in query
         return db.students.get(args.id);
     },
 
-    sayHello:(root,args,context,info) => `Hi ${args.name} GraphQL server says Hello to you!!`
+    sayHello: (root, args, context, info) => `Hi ${args.name} GraphQL server says Hello to you!!`,
+
+    setFavouriteColor: (root, args) => {
+        return "Your Fav Color is :" + args.color;
+    }
 }
 
 const Student = {
-    fullName:(root,args,context,info) => {
-       return root.firstName+":"+root.lastName
+    fullName: (root, args, context, info) => {
+        return root.firstName + ":" + root.lastName
     },
-    college:(root) => {
-       return db.colleges.get(root.collegeId);
+    college: (root) => {
+        return db.colleges.get(root.collegeId);
     }
- }
+}
 
 module.exports = { Query, Student }
