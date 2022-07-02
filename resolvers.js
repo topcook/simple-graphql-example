c351d70bbc70af1743107c62ca7f5bf6d451a089
topcook <codecooker1996@gmail.com>
2022-07-02 05:14:56 -0400

    mutation function returning object

--- resolvers.js
+++ resolvers.js
@@ -20,20 +20,33 @@ const Query = {
 }
 
 const Student = {
-    fullName: (root, args, context, info) => {
-        return root.firstName + ":" + root.lastName
-    },
+    // fullName: (root, args, context, info) => {
+    //     return root.firstName + ":" + root.lastName
+    // },
     college: (root) => {
         return db.colleges.get(root.collegeId);
     }
 }
 
 const Mutation = {
-    createStudent:(root,args,context,info) => {
-       return db.students.create({collegeId:args.collegeId,
-       firstName:args.firstName,
-       lastName:args.lastName})
+    createStudent: (root, args, context, info) => {
+        return db.students.create({
+            collegeId: args.collegeId,
+            firstName: args.firstName,
+            lastName: args.lastName
+        })
+    },
+
+    // new resolver function
+    addStudent_returns_object: (root, args, context, info) => {
+        const id = db.students.create({
+            collegeId: args.collegeId,
+            firstName: args.firstName,
+            lastName: args.lastName
+        })
+
+        return db.students.get(id)
     }
- }
+}
 
 module.exports = { Query, Student, Mutation }
