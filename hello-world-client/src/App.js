dae3517109d863529aa6c3ee5a9659ab24848d60
topcook <codecooker1996@gmail.com>
2022-07-04 18:32:45 -0400

    implemented apollo client

--- hello-world-client/src/App.js
+++ hello-world-client/src/App.js
@@ -1,107 +1,88 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 
-const loadGreeting = async () => {
+// apollo client
 
-  try {
-    const response = await axios.post(
-      'http://localhost:9000/graphql',
-      {
-        query: '{greeting}',
-      },
-      {
-        headers: {
-          'content-type': 'application/json',
-        }
+import {ApolloClient, HttpLink, InMemoryCache} from 'apollo-boost'
+import gql from 'graphql-tag'
+
+const endPointUrl = 'http://localhost:9000/graphql'
+const client = new ApolloClient({
+   link: new HttpLink({uri:endPointUrl}),
+   cache:new InMemoryCache()
+});
+
+async function loadStudentsAsync() {
+   const query = gql`
+   {
+      students{
+         id
+         firstName
+         lastName
+         college{
+            name
+         }
       }
-    );
-
-    console.log(response.data.data.greeting);
-    return response.data.data.greeting;
-
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const loadSayhello = async (name) => {
-
-  try {
-    const response = await axios.post(
-      'http://localhost:9000/graphql',
-      {
-        query: `{sayHello(name:"${name}")}`,
-      },
-      {
-        headers: {
-          'content-type': 'application/json',
-        }
-      }
-    );
-
-    console.log(response.data.data.sayHello);
-    return response.data.data.sayHello;
-  } catch (error) {
-    console.log(error);
-  }
-
+   }
+   `
+   const {data} = await client.query({query}) ;
+   return data.students;
 }
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { greetingMessage: '', sayHelloMessage: '', userName: '' }
-    this.updateName = this.updateName.bind(this);
-    this.showSayHelloMessage = this.showSayHelloMessage.bind(this);
-    this.showGreeting = this.showGreeting.bind(this);
-  }
-
-  showGreeting() {
-    loadGreeting().then(g => this.setState({ greetingMessage: g + " :-)" }))
-  }
-
-  showSayHelloMessage() {
-    const name = this.state.userName;
-    console.log(name)
-    loadSayhello(name).then(m => this.setState({ sayHelloMessage: m }))
-  }
-
-  updateName(event) {
-    this.setState({ userName: event.target.value })
-  }
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <br /><br />
-        <section>
-          <button id="btnGreet" onClick={this.showGreeting}>Greet</button>
-          <br /> <br />
-          <div id="greetingDiv">
-            <h1>{this.state.greetingMessage}</h1>
-          </div>
-        </section>
-
-        <hr />
-
-        <section>
-          Enter a name:<input id="txtName" type="text" onChange={this.updateName}
-            value={this.state.userName} />
-          <button id="btnSayhello" onClick={this.showSayHelloMessage}>SayHello</button>
-          <br />
-          user name is:{this.state.userName}    <br />
-          <div id="SayhelloDiv">
-            <h1>{this.state.sayHelloMessage}</h1>
-          </div>
-        </section>
-      </div>
-    );
-  }
+class  App  extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         students:[]
+      }
+      this.studentTemplate =  [];
+   }
+   async loadStudents() {
+      const studentData =  await loadStudentsAsync();
+      this.setState({
+         students: studentData
+      })
+      console.log("loadStudents")
+   }
+   render() {
+      return(
+         <div>
+            <input type = "button"  value = "loadStudents" onClick = {this.loadStudents.bind(this)}/>
+            <div>
+               <br/>
+               <hr/>
+               <table border = "3">
+                  <thead>
+                     <tr>
+                        <td>First Name</td>
+                        <td>Last Name</td>
+                        <td>college Name</td>
+                     </tr>
+                  </thead>
+                  
+                  <tbody>
+                     {
+                        this.state.students.map(s => {
+                           return (
+                              <tr key = {s.id}>
+                                 <td>
+                                    {s.firstName}
+                                 </td>
+                                 <td>
+                                    {s.lastName}
+                                 </td>
+                                 <td>
+                                    {s.college.name}
+                                 </td>
+                              </tr>
+                           )
+                        })
+                     }
+                  </tbody>
+               </table>
+            </div>
+         </div>
+      )
+   }
 }
 
 export default App;
